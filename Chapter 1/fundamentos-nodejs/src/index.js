f3b88afbbac74be6541b5671ae70298ad5a2f694
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-04-04 15:27:52 -0300

    Learning how to use and create http methods

--- Chapter 1/fundamentos-nodejs/src/index.js
+++ Chapter 1/fundamentos-nodejs/src/index.js
@@ -1,11 +1,48 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({
-        message: 'Hello World Ignite - Fundamentos'
-    });
+app.get("/courses", (req, res) => {
+    return res.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3"
+    ]);
+});
+
+app.post("/courses", (req, res) => {
+    return res.json([
+        "Curso 1", 
+        "Curso 2",
+        "Curso 3",
+        "Curso 4"
+    ]);
+});
+
+app.put("/courses/:id", (req, res) => {
+    return res.json([
+        "Curso 10", 
+        "Curso 2",
+        "Curso 3",
+        "Curso 4"
+    ]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+    return res.json([
+        "Curso 10", 
+        "Curso 20",
+        "Curso 30",
+        "Curso 40"
+    ]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+    return res.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 4"
+    ]);
 });
 
 app.listen(3333);
