38d0127f0491bc57a9fe76ed98eef8f7233f7e33
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-04-05 13:25:45 -0300

    A post for creating user

--- Chapter 2/FINAPI/src/index.js
+++ Chapter 2/FINAPI/src/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 
-app.arguments(express.json());
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidV4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+});
 
 
 app.listen(3333);
