b60fec6f6621c00f4ad6bd4d3f0d39c72ff299a7
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-04-18 15:03:55 -0300

    feat: Add route, repository and service to specifications

---
+++ Chapter II/retanlx/src/routes/specifications.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationRoutes = Router();
+const specificationRepository = new SpecificationsRepository();
+
+specificationRoutes.post("/", (req, res) => {
+    const { name, description } = req.body;
+    const createSpecificationService = new CreateSpecificationService(
+        specificationRepository
+    );
+
+    createSpecificationService.execute({ name, description });
+
+    return res.status(201).send();
+});
+
+export { specificationRoutes };
--- Chapter II/retanlx/src/server.ts
+++ Chapter II/retanlx/src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
-
-app.listen(3333, () => console.log("server is running"));
+app.use("/specifications", specificationRoutes);
+app.listen(3333);
