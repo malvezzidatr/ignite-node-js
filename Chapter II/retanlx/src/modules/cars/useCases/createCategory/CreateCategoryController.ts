958dbc34fe7037c56953b45aed2f8a8ef223b17f
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-04-18 15:42:48 -0300

    Creating a controller to list categories

--- Chapter II/retanlx/src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ Chapter II/retanlx/src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from "express";
 
-import { CreateCategoryUseCase } from "./CreateCategoryService";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 class CreateCategoryController {
     constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
     handle(request: Request, response: Response): Response {
         const { name, description } = request.body;
-
         this.createCategoryUseCase.execute({ name, description });
         return response.status(201).send();
     }
--- Chapter II/retanlx/src/modules/cars/useCases/createCategory/index.ts
+++ Chapter II/retanlx/src/modules/cars/useCases/createCategory/index.ts
@@ -1,6 +1,6 @@
 import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { CreateCategoryController } from "./CreateCategoryController";
-import { CreateCategoryUseCase } from "./CreateCategoryService";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 const categoriesRepository = new CategoriesRepository();
 const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
